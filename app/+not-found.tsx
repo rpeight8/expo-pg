6c74c1fcefbf2602ee8224fea29dff7dfab39535
rpeight8 <vitali.shuman96@gmail.com>
2025-02-04 11:31:12 +0300

    Fix title of not found

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function NotFoundScreen() {
   return (
     <>
-      {/* <Stack.Screen options={{ title: "Oops! Not Found" }} /> */}
+      <Stack.Screen options={{ title: "Oops! Not Found" }} />
       <View style={styles.container}>
         <Link href="/" style={styles.button}>
           Go back to Home screen!
